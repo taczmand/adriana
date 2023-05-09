e392ad931c934c4eb9b2197a192d5e543dd13234
taczmand <david.taczman@gmail.com>
2023-05-09 13:53:43 +0100

    Config

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 define("DB_TYPE", "mysql");
-define("DB_HOST", "taczmand.dyndns.hu");
-define("DB_NAME", "crudapi");
+define("DB_HOST", "");
+define("DB_NAME", "");
 define("DB_CHARSET", "utf8");
-define("DB_USER", "david");
-define("DB_PASS", "3556");
+define("DB_USER", "");
+define("DB_PASS", "");
 ?>
